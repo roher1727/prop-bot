e58d863db3f974528f6ff167ee6316571f3374f0
Alejandro Hernández <alejandrohernandez@Alejandros-Laptop.local>
2024-05-26 16:01:02 -0600

    Multiple videos

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Run `npm install` to install node modules
 
-Create a folder videos to store videos to sent
+Create a folder videos to store videos to sent. Example /videos/video_1.mp4
 
 Please provide your chrome path.
 
-
 Look for https://github.com/pedroslopez/whatsapp-web.js/pull/2816 to solve possible issues.
+
+Run main.js and pass the csv file name as argument `node main.js csvfilename.csv`
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const csv = require('csv-parser');
 const path = require('path');
+const process = require('process');
+
 
 // Crear una nueva instancia del cliente
 const client = new Client({
@@ -19,9 +21,9 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 client.once('ready', async () => {
     console.log('Client is ready!');
     const contacts = [];
-
+    const video_names = ['video_1.mp4','video_2.mp4','video_3.mp4'];
     // Leer el archivo CSV y enviar mensajes a cada número
-    fs.createReadStream('./contactos.csv')
+    fs.createReadStream(process.argv[2])
         .pipe(csv())
         .on('data', (row) => {
             contacts.push(row);
@@ -39,25 +41,27 @@ client.once('ready', async () => {
                     if (response.id.fromMe) {
                         console.log(`Mensaje enviado con éxito a ${row.number}`);
                     }
+                    
+                    for (const video_name of video_names){
+                        // Ruta del video a enviar
+                        const videoPath = path.join(__dirname, 'videos', video_name); // Reemplaza 'video_demo.mp4' con el nombre del archivo de video
 
-                    // Ruta del video a enviar
-                    const videoPath = path.join(__dirname, 'videos', 'video_demo.mp4'); // Reemplaza 'video_demo.mp4' con el nombre del archivo de video
+                        // Verificar el tamaño del archivo de video
+                        const stats = fs.statSync(videoPath);
+                        const fileSizeInMegabytes = stats.size / (1024 * 1024);
+                        console.log(`Tamaño del video: ${fileSizeInMegabytes.toFixed(2)} MB`);
 
-                    // Verificar el tamaño del archivo de video
-                    const stats = fs.statSync(videoPath);
-                    const fileSizeInMegabytes = stats.size / (1024 * 1024);
-                    console.log(`Tamaño del video: ${fileSizeInMegabytes.toFixed(2)} MB`);
+                        // Asegurarse de que el tamaño del video sea razonable (por ejemplo, menos de 16 MB)
+                        if (fileSizeInMegabytes <= 16) {
+                            // Leer el video
+                            const media = MessageMedia.fromFilePath(videoPath);
 
-                    // Asegurarse de que el tamaño del video sea razonable (por ejemplo, menos de 16 MB)
-                    if (fileSizeInMegabytes <= 16) {
-                        // Leer el video
-                        const media = MessageMedia.fromFilePath(videoPath);
-
-                        // Enviar video
-                        await client.sendMessage(chatId, media);
-                        console.log(`Video enviado con éxito a ${row.number}`);
-                    } else {
-                        console.log(`El video es demasiado grande para enviar a ${row.number}`);
+                            // Enviar video
+                            await client.sendMessage(chatId, media);
+                            console.log(`Video enviado con éxito a ${row.number}`);
+                        } else {
+                            console.log(`El video es demasiado grande para enviar a ${row.number}`);
+                        }
                     }
 
                 } catch (err) {
